f32a363b6ebe7cc8e34ab2b05bfb21399870fb50
AzamovMuhammadiso <100135819+AzamovMuhammadiso@users.noreply.github.com>
2022-02-25 17:58:35 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  const AddList = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
+
+  const AddTodo = () => {
+    setItems((e) => {
+      return [...e, input];
+    });
+    setInput("");
+  };
+
+  const DeleteTodo = (id) => {
+    setItems((e) => {
+      return e.filter((arr, index) => {
+        return index != id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container d-flex justify-content-center row">
+      <div className="text-title d-flex align-items-center justify-content-center ">
+        <h1 className="text-warning fs-1">Todo List</h1>
+      </div>
+      <div className="container text-input my-3 d-flex align-items-center justify-content-center">
+        <input
+          type="text"
+          className="px-2"
+          placeholder="write text..."
+          value={input}
+          onChange={AddList}
+        />
+        <div className="btn-group d-block mx-3">
+          <div
+            className="btn px-3 py-1 text-white text-align-center justify-content-center bg-success"
+            onClick={AddTodo}
+          >
+            Add Todo
+          </div>
+        </div>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <div
+            key={index + 1}
+            className="container item-container d-flex align-items-center justify-content-center"
+          >
+            <h3 className="item-text text-warning col-md-8">{item}</h3>
+            <div className="btn-group col-md-3">
+              <button className="px-3 py-1 text-white bg-danger" onClick={() => DeleteTodo(index)}>Add Todo</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
